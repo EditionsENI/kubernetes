d8aa4d6198e8e89a259f976a59b3c382114c2d84
Yannig Perré <yannig.perre@aios.sh>
2019-08-01 09:39:53 +0200

    Réorganisation et ménage fichiers du repository.

--- capacity/nodes-autoscaler/deploy-wp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for i in 1 2 3
-do
-  helm upgrade --install wordpress-$i stable/wordpress \
-               --namespace wp-$i                                                                                                                                               
-done
